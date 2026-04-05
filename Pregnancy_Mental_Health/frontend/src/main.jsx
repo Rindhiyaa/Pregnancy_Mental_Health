a13dda07a155c457dee828af5cb16310e4cf7b0f
Rindhiyaa <rindhiyaa.2201205@srec.ac.in>
2026-04-05 21:31:52 +0530

    Update main.jsx

--- Pregnancy_Mental_Health/frontend/src/main.jsx
+++ Pregnancy_Mental_Health/frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import App from "./App";
 
@@ -17,9 +17,9 @@ import "./styles/DashboardPage.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
